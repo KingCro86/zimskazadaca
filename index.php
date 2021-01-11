1b8e3f6d93cd12265566f9d6723b10582efaf0a4
KingCro86 <74135979+KingCro86@users.noreply.github.com>
2021-01-11 19:34:24 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,44 @@
+<!doctype html>
+<html class="no-js" lang="en">
+  <head>
+    <?php
+         include_once 'head.php';
+    ?>
+  </head>
+
+  <body style="background: white">
+
+  <div class="top-bar">
+  <div class="top-bar-left">
+    <ul class="dropdown menu" data-dropdown-menu>
+      <li class="menu-text">Glazba</li>
+      <li>
+        <a href="#">One</a>
+        <ul class="menu vertical">
+          <li><a href="#">Slike</a></li>
+          <li><a href="#">Glazba</a></li>
+          <li><a href="#">Izvođač</a></li>
+          <li><a href='#'>Hardstyle</a></li>
+        </ul>
+      </li>
+      <li><a href="login.php">Login</a></li>
+      
+    </ul>
+  </div>
+  <div class="top-bar-right">
+    <ul class="menu">
+      <li><input type="search" placeholder="tražilica"></li>
+      <li><button type="button" class="button">traži</button></li>
+    </ul>
+  </div>
+</div>
+
+OVO JE POČETNA STRANICA
+
+    <?php
+        include_once 'skripte.php';
+        
+    ?>
+
+  </body>
+</html>
---
+++ nadzornaploca.php
@@ -0,0 +1,49 @@
+<!doctype html>
+<html class="no-js" lang="en">
+  <head>
+    <?php
+         include_once 'head.php';
+    ?>
+  </head>
+
+  <body style="background:ligt blue">
+
+  <div class="top-bar">
+  <div class="top-bar-left">
+    <ul class="dropdown menu" data-dropdown-menu>
+     
+      
+      <li><a href="index.php">POČETNA</a></li>
+      
+    </ul>
+  </div>
+  <div class="top-bar-right">
+    <ul class="menu">
+      <li><input type="search" placeholder="TRAŽILICA"></li>
+      <li><button type="button" class="button">TRAŽI</button></li>
+    </ul>
+  </div>
+</div>
+
+
+
+<div class="card" style="width: 300px;">
+  <div class="card-divider">
+    This is a header
+  </div>
+  <img src="img/slika-1.png">
+  <div class="card-section">
+    <h4>This is a card.</h4>
+    <p>It has an easy to override visual style, and is appropriately subdued.</p>
+  </div>
+</div>
+
+    
+
+    <?php
+        include_once 'skripte.php';
+        
+    ?>
+
+  </body>
+</html>
